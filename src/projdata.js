a1bbf69d6023886210744b90c30704a66c1ec8f6
sumitbudaniyaa <sumit.k.budaniya@gmail.com>
2025-06-09 02:02:44 +0530

    changes

--- src/projdata.js
+++ src/projdata.js
@@ -1,4 +1,11 @@
 const projdata = [
+        {
+        title: "Feedo",
+        desc: "Built Full Stack QR food ordering app for restaurants.",
+        web: "https://consolefeedo.vercel.app/",
+        logo: "feedo..png",
+       
+    },
     {
         title: "Geni",
         desc: "Built a Web App for college and career guidance. ",
@@ -11,22 +18,7 @@ const projdata = [
         web: "https://suplyr.vercel.app/",
         logo: "suplyr-logo-light.png",
        
-    },
-    {
-        title: "Relvl",
-        desc: "Built Website for a Social Media Agency.",
-        web: "https://www.relvl.in",
-        logo: "iglogo-01.png",
-
-    },
-    {
-        title: "Amol",
-        desc: "Built Website for a Tofu Brand.",
-        web: "",
-        logo: "AMOL LOGO-01.png",
-        
-    },
-  
+    }
 ]
 
 export default projdata;
